6546307d959ffd7c0b076d084664c8eb154aa6e1
Allan <altonallan47@gmail.com>
2025-07-28 17:43:09 +0300

    Update the Supabase & Auth0 Key-URL Pair

--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,10 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
 <Auth0Provider
-		domain="dev-67afa2olaw5xjd2x.us.auth0.com"
-		clientId="YpYHyl81Zkn0n8x7y6oj8IjTlZe3bm9N"
+		domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+		// domain="dev-67afa2olaw5xjd2x.us.auth0.com"
+		// clientId="YpYHyl81Zkn0n8x7y6oj8IjTlZe3bm9N"
 		authorizationParams={{
 			redirect_uri: window.location.origin
 		}}
--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'; 
 
-const supabaseUrl = ''; // Replace with your Supabase URL
-const supabaseKey = ''; // Replace with your Supabase anon key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; 
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
