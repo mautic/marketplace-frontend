6234bb3d3db742c57c592095287ea6d5fdcc76b8
Allan <altonallan47@gmail.com>
2025-07-28 21:37:20 +0300

    Fix the Callback Mismatch Error
    This fixes the login error where Auth0 rejected the redirect URL with:
    "Callback URL mismatch."
    
    Updated the allowed URLs in the Auth0 dashboard to include:
    - http://localhost:3000
    - http://localhost:3000/callback
    
    So now local login works as expected.

--- src/index.jsx
+++ src/index.jsx
@@ -9,8 +9,6 @@ root.render(
 <Auth0Provider
 		domain={import.meta.env.VITE_AUTH0_DOMAIN}
         clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-		// domain="dev-67afa2olaw5xjd2x.us.auth0.com"
-		// clientId="YpYHyl81Zkn0n8x7y6oj8IjTlZe3bm9N"
 		authorizationParams={{
 			redirect_uri: window.location.origin
 		}}
