ba2deebbfd40c3a73495bbb346e6bfc18b89b5c0
Allan <altonallan47@gmail.com>
2025-08-03 08:56:27 +0300

    Reviews Placeholders

---
+++ src/components/reviews/ReviewItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewItem = () => {
+  return (
+    <div>
+      <p>A single review item.</p>
+    </div>
+  );
+};
+
+export default ReviewItem;
---
+++ src/components/reviews/ReviewsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewsList = () => {
+  return (
+    <div>
+      <p>Reviews will be displayed here.</p>
+    </div>
+  );
+};
+
+export default ReviewsList;
